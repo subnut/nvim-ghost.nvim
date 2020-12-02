bfc6e737f63d0fb6dd2b43c187e85479faa380fb
Subhaditya Nath <sn03.general@gmail.com>
2020-12-02 18:52:31 +0530

    v0.0.11: Fix argument-parsing

--- binary.py
+++ binary.py
@@ -16,7 +16,7 @@ import requests
 from simple_websocket_server import WebSocket
 from simple_websocket_server import WebSocketServer
 
-BUILD_VERSION = "v0.0.10"
+BUILD_VERSION = "v0.0.11"
 TEMP_FILEPATH = os.path.join(tempfile.gettempdir(), "nvim-ghost.nvim.port")
 WINDOWS = os.name == "nt"
 LOCALHOST = "127.0.0.1" if WINDOWS else "localhost"
@@ -128,12 +128,11 @@ class ArgParser:
                 # i.e. they have some default value
                 # e.g. --focus
                 if argument in self.argument_handlers_data:
-                    if argument not in self.argument_handlers_nodata:
-                        if index + 1 >= len(args):
+                    if index + 1 >= len(args):
+                        if argument not in self.argument_handlers_nodata:
                             sys.exit(f"Argument {argument} needs a value.")
                     else:
-                        continue
-                    self.argument_handlers_data[argument](args[index + 1])
+                        self.argument_handlers_data[argument](args[index + 1])
                 if argument in self.argument_handlers_nodata:
                     self.argument_handlers_nodata[argument]()
 
