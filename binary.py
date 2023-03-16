8bf6f96a2b6bcf59c458001876e7c2a2cb14afc6
Subhaditya Nath <sn03.general@gmail.com>
2023-03-16 15:18:23 +0530

    Release v0.4.1
    - Fix the bug that was fixed in v0.3.2 and reintroduced in v0.4.0
    - Reduce default verbosity of logs. We can get more verbose logs on
      demand by setting the environment variable NVIM_GHOST_VERBOSE_LOGGING

--- binary.py
+++ binary.py
@@ -19,7 +19,7 @@ import requests
 from simple_websocket_server import WebSocket
 from simple_websocket_server import WebSocketServer
 
-BUILD_VERSION: str = "v0.4.0"
+BUILD_VERSION: str = "v0.4.1"
 
 WINDOWS: bool = os.name == "nt"
 LOCALHOST: str = "127.0.0.1" if WINDOWS else "localhost"
