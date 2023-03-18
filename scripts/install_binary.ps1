bcebc1fe30a6926165594d5d6fa60a80487bea1f
Subhaditya Nath <sn03.general@gmail.com>
2023-03-19 02:47:18 +0530

    Bugfix and improve install_binary.ps1

--- scripts/install_binary.ps1
+++ scripts/install_binary.ps1
@@ -1,34 +1,28 @@
-Write-Output "Preparing to download nvim-ghost binary..."
+Write-Output "Preparing to download nvim-ghost binary"
 Set-Location (Resolve-Path -Path ((Split-Path $myInvocation.MyCommand.Path) + "\.."))
 
-$version = Get-Content "binary_version"
-$asset = "nvim-ghost-win64.zip"
-$binary = "nvim-ghost-binary.exe"
+$version  = Get-Content "binary_version"
+$archive  = "nvim-ghost-win64.zip"
+$binary   = "nvim-ghost-binary.exe"
 
-# Delete previous partial downloads
-Remove-Item -ErrorAction:Ignore "$asset"
-
-# Delete current binary
+Remove-Item -ErrorAction:Ignore "$archive"
 Remove-Item -ErrorAction:Ignore "$binary"
 Remove-Item -ErrorAction:Ignore "$binary.version"
-if (Test-Path $binary) {
-  echo "Binary still running"
-  while (Test-Path $binary) {
-    try {
-      Remove-Item -ErrorAction:Stop "$binary"
-    }
-    catch {
-      echo "Please run ':call nvim_ghost#helper#kill_server()' in neovim"
-      Start-Sleep -Seconds 1
-    }
+
+if (Test-Path "$binary") {
+  Write-Output "Binary still running"
+  Write-Output "Please run ':call nvim_ghost#helper#kill_server()' in neovim"
+  while (Test-Path "$binary") {
+    try { Remove-Item -ErrorAction:Stop "$binary" }
+    catch { Start-Sleep -Seconds 1 }
   }
 }
 
-echo "Downloading binary..."
+Write-Output "Downloading binary $version"
 Invoke-WebRequest `
-  -uri "https://github.com/subnut/nvim-ghost.nvim/releases/download/$version/$asset" `
-  -OutFile "$asset"
-Expand-Archive "$asset"
-Remove-Item "$asset"
+  -uri "https://github.com/subnut/nvim-ghost.nvim/releases/download/$version/$archive" `
+  -OutFile "$archive"
+Expand-Archive "$archive" -DestinationPath .
+Remove-Item "$archive"
 
 # vim: et sw=2 ts=2 sts=2
